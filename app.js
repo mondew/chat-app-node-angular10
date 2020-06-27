5ae9d345a739594dd30bd188ec83b8615d58d848
mondew <shabnam786mansoori@gmail.com>
2020-06-28 05:22:39 +0530

    initial commit

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var debug = require('debug')('angular2-nodejs:server');
 
-app.use(express.static(__dirname + '/dist/chat-app'));
+app.use(express.static(__dirname + 'index.html'));
 console.log(__dirname + '\dist\chat-app')
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '\dist\chat-app\index.html'));
-  });
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '\dist\chat-app\index.html'));
+//   });
 // app.set('/*', path.join(__dirname, 'index.html'));
 // app.set('view engine', 'hbs');
 
